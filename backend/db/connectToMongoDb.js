4681a5f8646ed908e981acc5a90846f07fc8cb9e
Rijas Afradi <rijrijas@gmail.com>
2024-06-04 11:23:18 +0530

    Fix file path for connectToMongoDB.js

--- backend/db/connectToMongoDb.js
+++ backend/db/connectToMongoDb.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectToMongoDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URI);
+    await mongoose.connect(process.env.MONGO_DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Error connecting to MongoDB", error.message);
