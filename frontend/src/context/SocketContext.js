ecfbe72a4a60b307b1682d95ce5e6911a74494da
Rijas Afradi M <109467723+Rijas2004@users.noreply.github.com>
2024-12-08 20:09:02 +0530

    Update SocketContext.js

--- frontend/src/context/SocketContext.js
+++ frontend/src/context/SocketContext.js
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chattnext.onrender.com", {
+      const socket = io("https://chatnextt.netlify.app", {
         query: {
           userId: authUser._id,
         },
