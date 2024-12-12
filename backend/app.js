dfb41753f9b25247a310015bf57b969dc00a58ed
Rijas Afradi <rijrijas@gmail.com>
2024-12-12 16:22:20 +0530

    vercel json

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://chatnextt.netlify.app",
+    origin: "https://chatnextt.vercel.app",
     credentials: true,
   })
 );
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://chatnextt.netlify.app"],
+    origin: ["https://chatnextt.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
